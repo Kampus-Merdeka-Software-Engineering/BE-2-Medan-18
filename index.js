cfdc4eac8e571d0cace973e0fb8c7a520878ac09
rossy-awan <kurniawan190703@gmail.com>
2023-12-05 15:31:17 +0700

    otw Deploy

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import cors from "cors";
 import { Sequelize } from "sequelize";
 import { DataTypes } from "sequelize";
 import bcrypt from "bcrypt"; // Import bcrypt
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT;
 const db = new Sequelize(
-    process.env.DATABSE_NAME,
-    process.env.DATABSE_USERNAME,
-    process.env.DATABSE_PASSWORD, {
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USERNAME,
+    process.env.DATABASE_PASSWORD, {
         host: process.env.DATABASE_HOST,
         dialect: "mysql",
         port: process.env.DATABASE_PORT,
